8055a67ac7333e0c2425d20897f8e388e1b7c5b9
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-04-04 16:27:45 -0400

    Created Person Class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(props){
+        this.name = props.name;
+        this.age = props.age;
+        this.location = props.location;
+        this.gender = props.gender;
+    }
+    
+    speak(){
+        console.log(`Hello my ${name} is Fred, I am from ${location}`);
+    }
+}
